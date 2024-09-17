7ff4ff5409b6ce790113b40e024730befd14e6c0
Jinge Wang <wjg172184@163.com>
2024-09-17 11:42:34 +0800

    Put data preparation to dataset.py

---
+++ dataset.py
@@ -0,0 +1,129 @@
+import json
+import random
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+
+class Dataset():
+    def __init__(self, input_path: str = 'data.json', batch_size: int = 16, block_size: int = 256, device: str = 'cpu'):
+        self.batch_size = batch_size
+        self.block_size = block_size
+        self.device = device
+
+        # Load poems data
+        poems = json.load(open(input_path, 'r', encoding='utf-8'))
+        random.shuffle(poems)
+        # Split the data into 5:3:2 for pretrain, instruction finetune, and alignment
+        pretrain_poems = poems[:int(len(poems)*0.5)]
+        finetune_poems = poems[int(len(poems)*0.5):int(len(poems)*0.8)]
+        alignment_poems = poems[int(len(poems)*0.8):]
+
+        # Reformat pretrain data
+        pretrain_text = []
+        for poetry in pretrain_poems:
+            paragraphs = '\n'.join(poetry['paragraphs'])
+            pretrain_text.append(f'{poetry['title']}\n{paragraphs}')
+        pretrain_text = '\n\n'.join(pretrain_text)
+        print('The whole pretrain data is a long text with all poems concatenated together. Here are the first 100 characters:')
+        print(pretrain_text[:100])
+
+        # Reformat instruction finetune data
+        finetune_texts = []
+        instruction = '請用以下題目寫一首詩'
+        instruction_label = '<INS>'
+        input_label = '<INP>'
+        response_label = '<RES>'
+        for poetry in finetune_poems:
+            paragraphs = '\n'.join(poetry['paragraphs'])
+            content = f'{instruction_label}{instruction}{input_label}{poetry["title"]}{response_label}{paragraphs}'
+            finetune_texts.append(content)
+        print('The instruction finetune data is a list of formatted texts. Here are the first item:')
+        print(finetune_texts[0])
+
+        # Create a vocabulary from all the characters appeared in the dataset
+        all_text = f'{pretrain_text}{"".join(finetune_texts)}\0'
+        characters = sorted(list(set(all_text)))
+        vocabulary_size = len(characters)
+        print(f'Dataset length: {len(all_text)}, vocabulary size: {vocabulary_size}')
+        # Create a mapping from characters to indices and vice versa
+        character_to_index = { character: index for index, character in enumerate(characters) }
+        index_to_character = { index: character for index, character in enumerate(characters) }
+        # Encode method to convert a text to a list of indices
+        encode = lambda text: [character_to_index[character] for character in text]
+        # Decode method to convert a list of indices back to a text
+        decode = lambda index_list: ''.join([index_to_character[index] for index in index_list])
+
+        # Train and test splits for pretrain data
+        pretrain_data = torch.tensor(encode(pretrain_text), dtype=torch.long)
+        # Split the data into 90% train and 10% validation
+        self.pretrain_train_data = pretrain_data[:int(0.9 * len(pretrain_data))]
+        self.pretrain_validation_data = pretrain_data[int(0.9 * len(pretrain_data)):]
+
+        # Train and test splits for instruction finetune data
+        finetune_data = [torch.tensor(encode(finetune_text), dtype=torch.long) for finetune_text in finetune_texts]
+        # Split the data into 90% train and 10% validation
+        print(len(finetune_data))
+        self.finetune_train_data = finetune_data[:int(0.9 * len(finetune_data))]
+        self.finetune_validation_data = finetune_data[int(0.9 * len(finetune_data)):]
+
+    def get_batch_pretrain(self, split: str):
+        """
+        Generate a batch of pretrain data
+
+        Args:
+        - split: 'train' or 'validation'
+        """
+        # Choose train or validation split
+        data = self.pretrain_train_data if split == 'train' else self.pretrain_validation_data
+        # Randomly choose the starting index of each item in the batch
+        start_indices = torch.randint(len(data) - self.block_size, (self.batch_size,))
+        # The input texts are all the characters in interval [start_index, start_index + block_size) for each item in the batch
+        inputs = torch.stack([data[index:index+self.block_size] for index in start_indices])
+        # The label texts are all the characters in interval [start_index + 1, start_index + block_size + 1) for each item in the batch.
+        # So, the label texts are the same as the input texts, but shifted by 1 character to the right.
+        labels = torch.stack([data[index+1:index+self.block_size+1] for index in start_indices])
+        # Move the tensors to the device and return
+        return inputs.to(self.device), labels.to(self.device)
+
+    def generate_batch_finetune(self, split: str):
+        """
+        Generate a batch of instruction finetune data
+
+        Args:
+        - split: 'train' or 'validation'
+        """
+        # Choose train or validation split
+        data = self.finetune_train_data if split == 'train' else self.finetune_validation_data
+
+        def process_batch(batch: list):
+            # All the inputs and labels are initialized to zeros of largest length
+            inputs = torch.zeros(len(batch), self.block_size, dtype=torch.long)
+            labels = torch.zeros(len(batch), self.block_size, dtype=torch.long)
+            for i, item in enumerate(batch):
+                # Assign the real values to the zeros-initialized tensors
+                available_length = len(item) if len(item) < self.block_size else self.block_size
+                inputs[i, :available_length] = item[:available_length]
+                labels[i, :available_length-1] = item[1:available_length]
+
+                # Mask all the remaining zeros by setting them to -100 (the loss function will ignore these tokens)
+                mask = labels[i] == 0
+                indices = torch.nonzero(mask).squeeze()
+                if indices.numel() > 1:
+                    # Exclude the first zero because it marks the end of the text
+                    labels[i, indices[1:]] = -100
+            return inputs, labels
+
+        # Initialize an empty list to store the batch
+        batch = []
+        for item in data:
+            batch.append(item)
+            # If the batch is full, process it and yield
+            if len(batch) >= self.batch_size:
+                inputs, labels = process_batch(batch)
+                batch = []
+                yield inputs.to(self.device), labels.to(self.device)
+        # If there are still remaining items, process them and yield
+        if len(batch) > 0:
+            inputs, labels = process_batch(batch)
+            yield inputs.to(self.device), labels.to(self.device)
--- run.py
+++ run.py
@@ -39,98 +39,12 @@ torch.manual_seed(1993)
 """
 Prepare Data
 """
-# Define the label formats in fine-tuning
-instruction = '請用以下題目寫一首詩'
-instruction_label = '<INS>'
-input_label = '<INP>'
-response_label = '<RES>'
-
-poems = json.load(open('data.txt', 'r', encoding='utf-8'))
-random.shuffle(poems)
-n = int(0.5 * len(poems))
-pretrain_poems = poems[:n]
-finetune_poems = poems[n:]
-
-pretrain_data = []
-finetune_data = []
-for poetry in pretrain_poems:
-  content = '\n'.join(poetry['paragraphs'])
-  pretrain_data.append(content)
-pretrain_data = '\n\n'.join(pretrain_data)
-for poetry in finetune_poems:
-  paragraphs = '\n'.join(poetry['paragraphs'])
-  content = f'{instruction_label}{instruction}{input_label}{poetry["title"]}{response_label}{paragraphs}'
-  finetune_data.append(content)
-
-# here are all the unique characters that occur in this text
-# Add a '\0' as terminator
-all_text = f'{pretrain_data}{"".join(finetune_data)}\0'
-chars = sorted(list(set(all_text)))
-vocab_size = len(chars)
-print(f'text_len={len(all_text)}, {vocab_size=}')
-# create a mapping from characters to integers
-stoi = { ch:i for i,ch in enumerate(chars) }
-itos = { i:ch for i,ch in enumerate(chars) }
-encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
-decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
-
-# Train and test splits for pretrain data
-pretrain_data = torch.tensor(encode(pretrain_data), dtype=torch.long)
-n = int(0.9*len(pretrain_data)) # first 90% will be train, rest val
-pretrain_train_data = pretrain_data[:n]
-pretrain_val_data = pretrain_data[n:]
-
-# Train and test splits for finetune data
-finetune_data = [torch.tensor(encode(text), dtype=torch.long) for text in finetune_data]
-n = int(0.9*len(finetune_data))
-finetune_train_data = finetune_data[:n]
-finetune_val_data = finetune_data[n:]
-
-
-def get_batch_pretrain(split):
-    # generate a small batch of data of inputs x and targets y
-    data = pretrain_train_data if split == 'train' else pretrain_val_data
-    ix = torch.randint(len(data) - block_size, (batch_size,))
-    x = torch.stack([data[i:i+block_size] for i in ix])
-    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
-    x, y = x.to(device), y.to(device)
-    return x, y
-
-def generate_batch_finetune(split):
-    data = finetune_train_data if split == 'train' else finetune_val_data
-    batch_list = []
-    for tensor in data:
-        batch_list.append(tensor)
-        if len(batch_list) >= batch_size:
-            # Get the maximum length in this batch
-            max_len = max([len(x)+1 for x in batch_list])
-            # Padding all the tensor to the max_len
-            x = torch.zeros(batch_size, block_size, dtype=torch.long)
-            y = torch.zeros(batch_size, block_size, dtype=torch.long)
-            for i, tensor in enumerate(batch_list):
-                available_length = len(tensor) if len(tensor) < block_size else block_size
-                x[i, :available_length] = tensor[:available_length]
-                y[i, :available_length-1] = tensor[1:available_length]
-
-                # New: Replace all but the first 0 in y by -100
-                mask = y[i] == 0
-                indices = torch.nonzero(mask).squeeze()
-                if indices.numel() > 1:
-                    y[i, indices[1:]] = -100
-
-            batch_list = []
-            x, y = x.to(device), y.to(device)
-            yield x, y
-
-# Generate a batch
-batch = generate_batch_finetune('train')
-batch = next(batch)
-print(batch[0].shape)
-print(batch[1].shape)
-print(batch[0])
-print(batch[1])
-
-
+from dataset import Dataset
+dataset = Dataset('data.json', batch_size, block_size, device)
+print('Check a batch of pretrain data:')
+print(dataset.get_batch_pretrain('train'))
+print('Check a batch of finetune data:')
+print(next(dataset.generate_batch_finetune('train')))
 
 @torch.no_grad()
 def estimate_loss_eval(stage='pretrain'):
@@ -231,11 +145,11 @@ class BigramLanguageModel(nn.Module):
     def __init__(self):
         super().__init__()
         # each token directly reads off the logits for the next token from a lookup table
-        self.token_embedding_table = nn.Embedding(vocab_size, dim_embedding)
+        self.token_embedding_table = nn.Embedding(vocabulary_size, dim_embedding)
         self.position_embedding_table = nn.Embedding(block_size, dim_embedding)
         self.blocks = nn.Sequential(*[Block(dim_embedding, n_head=num_head) for _ in range(num_layer)])
         self.ln_f = nn.LayerNorm(dim_embedding) # final layer norm
-        self.lm_head = nn.Linear(dim_embedding, vocab_size)
+        self.lm_head = nn.Linear(dim_embedding, vocabulary_size)
 
     def forward(self, idx, targets=None):
         B, T = idx.shape
